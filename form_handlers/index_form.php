7ed7ed27f2c7b92fd9bd39f8e8e1dbc86e3493ca
Jack Touhey <jt112112@gmail.com>
2026-04-02 12:56:40 +1000

    feat: Now inserting form data into database

--- form_handlers/index_form.php
+++ form_handlers/index_form.php
@@ -1,11 +1,49 @@
 <?php
- 
+ session_start();
  if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $title = htmlspecialchars($_POST["title"]);   
     $author = htmlspecialchars($_POST["author"]);
     $rating = htmlspecialchars($_POST["rating"]);
+    $comments = htmlspecialchars($_POST["comments"]);
 
-    header("Location: ../index.php");
-} else {
+    echo "Title: ".$title;
+    echo "Author: ".$author;
+    echo "Rating: ".$rating;
+    echo "Comments: ".$comments;
+
+    echo "<br>";
+
+    $host = "localhost";
+    $port = "42069";
+    $dbname = "alex_archive_db";
+    $user = "root";
+    $password = "root";
+
+    $conn = pg_connect("host = $host port = $port dbname = $dbname user = $user password = $password");
+
+    if (!$conn) {
+        echo "Connection Error: ".pg_last_error();
+    } else {
+        echo "Connection successful";
+    }
+
+    $query = "INSERT INTO books (title, author, rating, comments) VALUES ($1, $2, $3, $4)";
+    $values = [
+        $title,
+        $author,
+        $rating,
+        $comments
+        ];
+
+    pg_prepare($conn, "insert_query", $query);
+    $result = pg_execute($conn, "insert_query", $values);
+
+    $success = true;
+    if (!$result) {
+        $success = false;
+    } 
+
+    $_SESSION["insertSuccess"] = $success;
+
     header("Location: ../index.php");
 }
--- index.php
+++ index.php
@@ -21,9 +21,24 @@
 
             <label for="rating">Rating</label>
             <input id="rating" type="number" name="rating" placeholder="Rating...">
+            <br/>
+
+            <label for="comments">Comments:</label>
+            <textarea id="comment" name="comments" rows="5" cols="25"></textarea>
 
             <button type="submit">Submit</button>
         </form>
+        <br/>
+        <?php 
+        session_start();
+            if (isset($_SESSION["insertSuccess"])) {
+                if($_SESSION["insertSuccess"]) {
+                    echo "Successfully inserted book";
+                } else {
+                    echo "Lmao that shit did NOT work";
+                }
+            }
+        ?>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
