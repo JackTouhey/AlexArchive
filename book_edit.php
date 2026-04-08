0c70156fdade4177263e7e74a095278bdb99118a
Jack Touhey <jt112112@gmail.com>
2026-04-08 14:39:01 +1000

    feat: Added new input styling to new_book

--- book_edit.php
+++ book_edit.php
@@ -75,33 +75,6 @@
 
     </div>
 
-    <script>
-        const container = document.getElementById('starRating');
-        const input = document.getElementById('ratingInput');
-        let currentRating = +document.getElementById('ratingInput').value || 0;
-
-        container.addEventListener('mouseleave', () => {
-            document.querySelectorAll('.star-half-wrap').forEach(s => {
-                s.classList.toggle('lit', +s.dataset.value <= currentRating);
-            });
-        });
-
-        container.addEventListener('mouseover', e => {
-            const star = e.target.closest('.star-half-wrap');
-            if (!star) return;
-            const v = +star.dataset.value;
-            document.querySelectorAll('.star-half-wrap').forEach(s => {
-                s.classList.toggle('lit', +s.dataset.value <= v);
-            });
-        });
-
-        container.addEventListener('click', e => {
-            const star = e.target.closest('.star-half-wrap');
-            if (!star) return;
-            currentRating = +star.dataset.value;
-            input.value = currentRating;
-            console.log('Clicked, rating set to:', currentRating);
-        });
-    </script>
+    <script src="resources/rating_stars.js"></script>
 </body>
 </html>
--- new_book.php
+++ new_book.php
@@ -5,40 +5,80 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alex Archive</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="resources/header.css" rel="stylesheet">
+    <link href="resources/book_rating.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/c057f0eb33.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
+    <?php
+        include __DIR__ . "\session_utils\generic_utils.php";
+        $rating = 10;
+    ?>
 
-    <main>
-        <?php
-            include __DIR__ . "\session_utils\generic_utils.php";
-        ?>
-        <form action="form_handlers/submit_new_book.php" method="post">
-            <label for="title">Title:</label>
-            <input id="title" type="text" name="title" placeholder="Title...">
-            <br/>
+    <div class="col-12 d-flex">
 
-            <label for="author">Author:</label>
-            <input id="author" type="text" name="author" placeholder="Author...">
-            <br/>
+        <div class="col-1"></div>
 
-            <label for="rating">Rating</label>
-            <input id="rating" type="number" name="rating" placeholder="Rating...">
-            <br/>
+        <div class="col-10 shadow rounded-1 d-flex flex-column flex-wrap">
+            <!-- Header - would be nice to have this as a component -->
+            <div class="row d-flex flex-row justify-content-evenly">
+                <div class="fs-2 col-11 mp-2 d-flex justify-content-center">Alex's Archive</div>
+                <div class="col-1 d-flex flex-row align-items-center me-0">
+                    <a href="library.php"><i class="fa-solid fa-house fa-lg icon"></i></a>
+                    <i class="fa-solid fa-magnifying-glass fa-lg mp-2 icon"></i>
+                    <a href="new_book.php"><i class="fa-regular fa-square-plus fa-xl mp-2 icon"></i></a>
+                </div>
+            </div>
 
-            <label for="comments">Comments:</label>
-            <textarea id="comment" name="comments" rows="5" cols="25"></textarea>
-            <br/>
+            <!-- Content -->
+            <div class="col-12 d-flex flex-row">
 
-            <label for="colour">Colour</label>
-            <input type="color" id="colour" name="colour" value="<?= getRandomColour() ?>">
+                <!-- Book Details -->
+                <form action="form_handlers/submit_new_book.php" method="post" class="col-8 d-flex flex-column justify-content-start p-4">
+                    <input type="hidden" name="rating" id="ratingInput" value="<?= $rating ?>">
+                    
+                    <input id="title" type="text" name="title" class="fs-1 fw-bold form-control-lg mb-2 border-0 border-bottom border-secondary" placeholder="Title">
+                    <input id="author" type="text" name="author" class="fs-4 form-control-lg mb-2 border-0 border-bottom border-secondary" placeholder="Author">
 
-            <button type="submit">Submit</button>
-        </form>
-        <br/>
-        <a href="library.php">home</a>
-    </main>
 
+                    <div class="d-flex flex-row justify-content-start" id="starRating">
+                        <?php for ($i = 1; $i <= 10; $i += 2): ?>
+                            <div class="star-container <?=  $i == 1 ? 'me-4' : 'mx-4' ?>">
+                                <img src="resources/images/half-star.svg"
+                                    class="star-half-wrap pointer <?= $i <= $rating ? 'lit' : '' ?>"
+                                    data-value="<?= $i ?>">
+                                <img src="resources/images/half-star.svg"
+                                    class="star-half-wrap mirrored pointer <?= ($i + 1) <= $rating ? 'lit' : '' ?>"
+                                    data-value="<?= $i + 1 ?>">
+                            </div>
+                        <?php endfor; ?>
+                    </div>
+                    
+                    <div class="col-12" style="height: 2.5rem;"></div>
+
+                    <div class="d-flex flex-row align-items-center ">
+                        <textarea id="comment" name="comments" rows="5" cols="155" class="border-0 border-bottom border-secondary p" placeholder="Comments" ></textarea>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center p-1">
+                        <div class="fs-4">Colour:</div>
+                        <input type="color" id="colour" name="colour" class="my-5 ms-3" value="<?= getRandomColour() ?>">
+
+                    </div>
+                    
+
+                    <button type="submit" class="btn btn-primary rounded-pill mt-2">Submit</button>
+                </form>
+
+            </div>
+        </div>
+
+        <div class="col-1"></div>
+
+    </div>
+
+    <script src="resources/rating_stars.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
